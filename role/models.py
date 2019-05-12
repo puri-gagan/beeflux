9b1e1a0fec50c96f378d56139a5127588f81242b
gagan <gaganpuri2055@gmail.com>
2019-05-12 14:16:24 +0545

    user role model is created

--- role/models.py
+++ role/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User, Group
+
+# Create your models here.
+
+class UserRole(models.Model):
+    user = models.OneToOneField(User,related_name="user_roles",on_delete=models.CASCADE)
+    company = models.ForeignKey('Company', null=True,blank=True,on_delete=models.CASCADE)
+    group = models.ForeignKey(Group,related_name="userrole_group",on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.group.name
+    
+    class Meta:
+        db_table = 'userrole'
